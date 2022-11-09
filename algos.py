b947483e4c8e1ced78267f835395b8f6c9784de4
Paula Namwanje <namwanjep@gmail.com>
2022-11-08 20:46:04 -0500

    challenge 2

--- algos.py
+++ algos.py
@@ -1,6 +1,4 @@
 # Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
-
-
 #Example
 # Input: nums = [0,1,0,3,12]
 # Output: [1,3,12,0,0]
@@ -16,4 +14,13 @@ def moveZeros(nums):
     return res
 print(moveZeros([0,1,0,3,0,12]))
 
+# Given an array of integers, find the sum of its elements.
+# For example, if the array = [1,2,3] returns 6
+
+def listSum(listInput):
+    sum = 0
+    for i in range(len(listInput)):
+        sum += listInput[i]
+    return sum
+print(listSum([1,2,3]))
 
